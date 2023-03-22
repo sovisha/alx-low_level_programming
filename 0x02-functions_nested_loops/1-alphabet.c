3dea0f27329b3d95b9e99b7ebe6ade5a51641022
sovisha <soviget@gmail.com>
2023-03-22 04:11:33 -0700

    a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
-* print_alphabet -  print alphabet in lowercase.
-*
-* Return: Always 0.
-*/
-
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
-	char i;
+	int i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
+	for (i = 97; i <= 122; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
-
-	return (0);
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	void print_alphabet();
+	print_alphabet();
 
 	return (0);
 }
