bcaad7694065e8f503693a5fdf11b9e1fb838752
sovisha <soviget@gmail.com>
2023-06-23 12:31:54 -0700

    lib

--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,1 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _memset - fills memory with a constant byte.
+ * @n: bytes filled
+ * @b: constant byte
+ * @s: memory area
+ * Return: pointer to dest
+ */
 char *_memset(char *s, char b, unsigned int n)
+{
+        unsigned int i;
+
+        for (i = 0; i < n; i++)
+                *(s + i) = b;
+
+        return (s);
+}
--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -1,1 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        unsigned int i;
+
+        for (i = 0; i < n; i++)
+                *(dest + i) =  *(src + i);
+
+        return (dest);
+}
